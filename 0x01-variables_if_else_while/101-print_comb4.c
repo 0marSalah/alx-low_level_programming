6207c79ad214640c3821f57b11bd09d1d401a66f
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-17 17:08:11 +0200

    ff

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,23 @@
 int main(void)
 {
 int i, j, k;
-for (i = 0; i <= 7; i++) {
-for (j = i + 1; j <= 8; j++) {
-for (k = j + 1; k <= 9; k++) {
+for (i = 0; i <= 7; i++)
+{
+for (j = i + 1; j <= 8; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
 putchar('0' + i);
 putchar('0' + j);
 putchar('0' + k);
-if (i != 8 || j != 9) {
+if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
 }
 }
 }
 }
-return 0;
+putchar('\n');
+return (0);
 }
