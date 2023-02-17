66f5b8f7e76805155350095b7fdb1aa3d3dee234
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-17 15:44:33 +0200

    g

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int i = '0';
+int i = '0';
 
-  while(i<='9')
-  {
-    putchar(i);
-    i++;
-  }
-  putchar('\n');
-  return (0);
+while(i<='9')
+{
+  putchar(i);
+  i++;
+}
+putchar('\n');
+return (0);
 }
