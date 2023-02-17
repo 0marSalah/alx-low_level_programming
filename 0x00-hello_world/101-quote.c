02a71623459b0261c3592d674ea2b9f4068da4b1
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-17 10:44:19 +0200

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful");
-fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
