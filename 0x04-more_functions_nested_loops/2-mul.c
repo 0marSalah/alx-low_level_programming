2228d3ed1d0b2baa82e5c2c5334c4cbe8c26ba58
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-23 21:34:52 +0200

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _isdigit - Returns 1 if c is a digit
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
+**/
+int mul(int a, int b)
+{
+return (a * b);
+}
