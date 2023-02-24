069166ee616a94eb5619bdb6bc4429148fc89ee6
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-24 15:27:41 +0200

    fix factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-
 /**
  * main -  checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	long x, y;
+	long num = 612852475143;
+	long i = 2;
+	long max_factor = 1;
 
-	x = 612852475143;
-
-	for (y = 2; x > y; y++)
+	while (num > 1)
 	{
-		while (x % y == 0)
+		if (num % i == 0)
 		{
-			x = x / y;
+			max_factor = i;
+			num /= i;
+			while (num % i == 0)
+			{
+					num /= i;
+			}
 		}
+		i++;
 	}
-	printf("%lu", y);
-	putchar('\n');
-	return (0);
+
+	printf("%ld\n", max_factor);
+
+	return 0;
 }
