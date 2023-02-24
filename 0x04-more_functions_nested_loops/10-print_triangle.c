df6f68c77df56aad84d2921aa16ed108c4e10de4
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-24 14:10:28 +0200

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,20 @@
 
 void print_triangle(int size)
 {
-	int l, c, s;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	for (l = 0; l < size; l++)
-	{
-		for (c = 0; c < size; c++)
-		{
-			s = size - l - 1;
-			if (c < s)
-			{
-				_putchar(' ');
-			}
-			else
-				_putchar('#');
-		}
-		_putchar('\n');
-	}
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if ((i + j) <= size) 
+{
+printf(" ");
+} 
+else 
+{
+printf("*");
+}
+}
+printf("\n");
+}
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -22,7 +22,6 @@ void print_diagonal(int n)
 			{
 				_putchar('\\');
 			}
-
 		}
 		_putchar('\n');
 	}
