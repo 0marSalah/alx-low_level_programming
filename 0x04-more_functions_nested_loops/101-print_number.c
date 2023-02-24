b44995b824ae806c30af754059360d5e5378d149
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-24 15:47:29 +0200

    finally

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,48 @@
  */
 void print_number(int n)
 {
-	if (n < 0) {
+	int i;
+	int max;
+	int minVal = n + 1 == -2147483640 - 7 ? -1 : 0;
+	int rem = n < 0 ? 0 - n + minVal : n;
+
+	max = 1;
+	max *= 100000;
+	max *= 100000;
+
+	if (n < 0)
 		_putchar('-');
-		n = -n;
+
+	for (i = 9; i >= 0; i--)
+	{
+		int power = b10_pow(i);
+
+		if (rem > power - 1 && !(i == 0 && minVal < 0))
+			_putchar((int)((rem / power) % 10) + '0');
+		else if (i == 0 && minVal >= 0)
+			_putchar((int)((rem / power) % 10) + '0');
+		if (i == 0 && minVal < 0)
+			_putchar('8');
 	}
-	if (n / 10) {
-		print_number(n / 10);
+}
+
+/**
+ * b10_pow - Computes the power of 10 to the given index
+ * @idx: The index to which 10 is raised to.
+ *
+ * Return: A power of 10
+ */
+int b10_pow(int idx)
+{
+	int i;
+	int result = 1;
+
+	if (idx < 0)
+		return (0);
+
+	for (i = 0; i < idx; i++)
+	{
+		result *= 10;
 	}
-	_putchar(n % 10 + '0');
+	return (result);
 }
