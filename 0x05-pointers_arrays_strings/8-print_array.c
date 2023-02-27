8cd269e0496de0c7fb419a38ab1af2f22e8b9ebf
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 23:52:12 +0200

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -26,13 +26,13 @@ void print_array(int *a, int n)
 {
 int i;
 for (i = 0; i < n; i++)
-if (i != (_strlen(a) - 1))
+if (i == (_strlen(a) - 1))
 {
-printf("%d, ", a[i]);
+printf("%d ", a[i]);
 }
 else
 {
-printf("%d ", a[i]);
+printf("%d, ", a[i]);
 }
 printf('\n');
 }
