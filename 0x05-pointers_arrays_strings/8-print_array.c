31c5f2a022a061b04e3cc233c9ee1d855dea364c
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 23:28:32 +0200

    init

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line.
+ * character, followed by a new line
+ * @str: pointer to a string
+ * Return: void
+**/
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+printf("%d, ", a[i]);
+printf("\n");
+}
