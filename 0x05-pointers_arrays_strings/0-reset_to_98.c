312a842e0f66e64c960abc6c8cfdc14d550f0199
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 19:35:52 +0200

    init 0 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Returns 1 if c is a digit
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
+ **/
+
+void reset_to_digit(int *n)
+{
+  *n = 98;
+}
