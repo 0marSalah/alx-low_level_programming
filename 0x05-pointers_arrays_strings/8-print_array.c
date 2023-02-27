87e117c1c5fd160bf0d65c810484c4b2faca8c67
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 23:41:59 +0200

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 /**
+* _strlen - function that returns the length of a string.
+* @s : s is a character
+* Return: value is i
+**/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
+
+/**
  * puts_half - function that prints half of a string, followed by a new line.
  * character, followed by a new line
  * @str: pointer to a string
