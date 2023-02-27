7dfadafa9b35d24be4548fa65230aa2bc697be03
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 22:13:41 +0200

    fix

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of a string.
+* @s : s is a character
+* Return: value is i
+**/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * character, followed by a new line
+ * @str: pointer to a string
+ * Return: void
+**/
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = _strlen(str) / 2; i < _strlen(str); i++)
+	if (i % 2 == 0)
+		_putchar(str[i]);
+	_putchar('\n');
+}
