4394673cc2d93d0ca9eb56125151a2c140a42627
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 23:55:40 +0200

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,20 +2,6 @@
 #include <stdio.h>
 
 /**
-* _strlen - function that returns the length of a string.
-* @s : s is a character
-* Return: value is i
-**/
-
-int _strlen(char *s)
-{
-int len = 0;
-while (*s++)
-len++;
-return (len);
-}
-
-/**
  * puts_half - function that prints half of a string, followed by a new line.
  * character, followed by a new line
  * @str: pointer to a string
@@ -26,7 +12,7 @@ void print_array(int *a, int n)
 {
 int i;
 for (i = 0; i < n; i++)
-if (i == (_strlen(a) - 1))
+if (i == n)
 {
 printf("%d ", a[i]);
 }
