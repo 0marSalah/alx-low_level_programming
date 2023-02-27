833a45963a112dee54c62be0d9cc10e6fdefb9ba
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 21:59:01 +0200

    init

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,13 @@
 * @s: char
 * Return: string
 **/
-void rev_string(char *s)
+void puts2(char *str)
 {
 int i = 0;
-int j;
 
-while (s[i] != '\0' && i % 2 == 0)
+while (str[i] != '\0' && i % 2 == 0)
 {
-_putchar(s[i]);
+_putchar(str[i]);
 i++;
 }
 }
