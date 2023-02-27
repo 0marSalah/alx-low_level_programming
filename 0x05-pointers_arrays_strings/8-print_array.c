6b0a74668d67026ecd71cd14726d0a853acb953d
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 23:49:29 +0200

    joj

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -26,6 +26,13 @@ void print_array(int *a, int n)
 {
 int i;
 for (i = 0; i < n; i++)
+if (i != (_strlen(a) - 1))
+{
 printf("%d, ", a[i]);
-printf("\n");
+}
+else
+{
+printf("%d ", a[i]);
+}
+_putchar('\n');
 }
