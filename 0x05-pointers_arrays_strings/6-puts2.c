6fccdb113cb9d74674c939ccabba469617bfa302
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 22:02:03 +0200

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,12 @@
 **/
 void puts2(char *str)
 {
-int i = 0;
+	int i;
+	int size = _strlen(str);
 
-while (str[i] != '\0')
-{
-if( i % 2 == 0)
-_putchar(str[i]);
-i++;
-}
+	for (i = 0; i < size; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
