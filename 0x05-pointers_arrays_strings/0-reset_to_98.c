efe8840d1eb67a07575d1f4d74fcd1dcd8a670e4
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-28 00:21:16 +0200

    lsm

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 * reset_to_98 -  takes a pointer to an int
 * @n: take an int
 * Return: int
 **/
-void reset_to_digit(int *n)
+
+void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
