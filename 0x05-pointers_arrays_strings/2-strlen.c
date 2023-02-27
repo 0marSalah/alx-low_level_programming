199ab052f8581516a6398f52f3e276d6a054b1b2
OmarSalah1911 <omar.salah1597@gmail.com>
2023-02-27 20:21:50 +0200

    fixeeeeeeeeeeeeeeeeeed

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,13 @@
 /**
 * _strlen return string length
 * @str: string parameter
-* return length of string 
+* return length of string
 **/
 
 int _strlen(char *s)
 {
 int len = 0;
 while (*s++)
-  len++;
-return len;
+len++;
+return (len);
 }
