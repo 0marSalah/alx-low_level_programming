a13d86642ca2b2a7ffe0b8c0ba21af766742f5ce
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 15:21:02 +0200

    with while

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,27 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	// for (i = 0; i < n && src[i] != '\0'; i++)
+	// {
+	// 	dest[i] = src[i];
+	// }
+	// for ( ; i < n; i++)
+	// {
+	// 	dest[i] = '\0';
+	// }
+	// return (dest);
+	while (i != '\0')
 	{
-		dest[i] = src[i];
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+		i++;
 	}
-	for ( ; i < n; i++)
+	while (i < n)
 	{
-		dest[i] = '\0';
+		dest[i] = "\0";
+		i++;
 	}
 	return (dest);
 }
