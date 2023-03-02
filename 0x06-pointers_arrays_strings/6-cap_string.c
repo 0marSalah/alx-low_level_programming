ae5bd5b371914898f4ff5bca782fdf70b8a5188e
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 19:39:52 +0200

    bettttttttttyt

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,17 +7,18 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-    int cap_next = 1;
-    while (str[i] != '\0') {
-        if (cap_next && str[i] >= 'a' && str[i] <= 'z') {
-            str[i] -= ('a' - 'A');
-        }
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') {
-            cap_next = 1;
-        } else {
-            cap_next = 0;
-        }
-        i++;
-    }
+	int cap_next = 1;
+	while (str[i] != '\0') {
+		if (cap_next && str[i] >= 'a' && str[i] <= 'z') {
+			str[i] -= ('a' - 'A');
+		}
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			cap_next = 1;
+		} else {
+			cap_next = 0;
+		}
+		i++;
+	}
 	return (str);
 }
