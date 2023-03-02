74696d9592fbda344a35041c596a288c11b2fb0b
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 14:52:34 +0200

    fix betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,12 +8,15 @@
 * Return: a pointer to the resulting string dest
 **/
 
-char *_strncat(char *dest, char *src, int n) {
+char *_strncat(char *dest, char *src, int n) 
+{
 	int dest_len = 0;
-	while (dest[dest_len] != '\0') {
+	while (dest[dest_len] != '\0') 
+	{
 			dest_len++;
 	}
-	for (int i = 0; i < n && src[i] != '\0'; i++) {
+	for (int i = 0; i < n && src[i] != '\0'; i++) 
+	{
 			dest[dest_len + i] = src[i];
 	}
 	dest[dest_len + n] = '\0';
