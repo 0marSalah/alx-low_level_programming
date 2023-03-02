e35ce27308e3e419167a47983fca8e2716042552
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 23:36:05 +0200

    ha3

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,35 +9,44 @@
  *
  * Return: If result can be stored in r, then r, otherwise 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int r_index)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-  int num, tens = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-  for (; *n1 && *n2; n1--, n2--, r_index--)
-  {
-    num = (*n1 - '0') + (*n2 - '0');
-    num += tens;
-    *(r + r_index) = (num % 10) + '0';
-    tens = num / 10;
-  }
-  for (; *n1; n1--, r_index--)
-  {
-    num = (*n1 - '0') + tens;
-    *(r + r_index) = (num % 10) + '0';
-    tens = num / 10;
-  }
-  for (; *n2; n2--, r_index--)
-  {
-    num = (*n2 - '0') + tens;
-    *(r + r_index) = (num % 10) + '0';
-    tens = num / 10;
-  }
-  if (tens && r_index >= 0)
-  {
-    *(r + r_index) = (tens % 10) + '0';
-    return (r + r_index);
-  }
-  else if (tens && r_index < 0)
-    return (0);
-  return (r + r_index + 1);
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
