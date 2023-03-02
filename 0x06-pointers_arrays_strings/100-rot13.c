4ad867c240001d3c7b133f43a431d12252397325
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 20:49:14 +0200

    github sol

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,27 @@
 #include "main.h"
 
 /**
-* *rot13 - a function that encodes a string into rot13
-* @str: string
-* Return: string
-**/
-
-char *rot13(char *str)
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
 {
 	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklm";
-
-	i = 0;
-	j = 0;
-
-	while (str[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (j < 26 && alpha[j] != str[i] && alpha[j] - 32 != str[i])
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			j++;
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
 		}
-
-		if (j < 26)
-		{
-			str[i] = rot13[j];
-		}
-
-		i++;
-		j = 0;
 	}
-	return (str);
+	return (s);
 }
