15e23114647e3a66667123504044c12f450c133e
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 17:02:17 +0200

    while

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 != *s2)
 		{
@@ -20,6 +20,8 @@ int _strcmp(char *s1, char *s2)
 		{
 			break;
 		}
+		s1++;
+		s2++;
 	}
 
 	if (*s1 == *s2)
