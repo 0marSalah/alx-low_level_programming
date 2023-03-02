d825700ae71fbd90f4150015fff7ef7f65ec28b9
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 16:55:59 +0200

    last

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,28 +9,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1_length = 0;
-	int i = 0;
-
-	while (s1[s1_length] != '\0')
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
 	{
-		s1_length++;
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		else if (*s1 == '\0' || *s2 == '\0')
+		{
+			break;
+		}
 	}
 
-	for (i = 0; i < s1_length; i++)
+	if (*s1 == *s2)
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-15);
-		}
+		return (0);
 	}
-	return (0);
+	else
+	{
+	return (*s1 - *s2);
+	}
 }
