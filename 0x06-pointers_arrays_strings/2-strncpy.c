6f4c42b8ead98b24e134c993994c10ffafda5054
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 15:16:59 +0200

    cpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *_strncat - function that copies a string.
+* @dest: string to be appended
+* @src: string to be appended from
+* @n: number of bytes from src
+* Return: a pointer to the resulting string dest
+**/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
