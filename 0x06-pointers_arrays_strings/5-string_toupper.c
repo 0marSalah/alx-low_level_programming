1f4f6c99aa1935f9fe7447b95d2055be59ad702c
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 19:04:42 +0200

    init

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
-* *string_toupper - changes all lowercase letters of a string to uppercase
+* *cap_string - changes all letters of a string to uppercase
 * @str: string
 * Return: string
 **/
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-		}
+		str[i] = str[i] - 32;
 	}
 	return (str);
 }
