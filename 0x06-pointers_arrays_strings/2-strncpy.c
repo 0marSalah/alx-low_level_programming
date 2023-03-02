05caa25684e0652791087a295a2f94dbce461af8
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 15:24:56 +0200

    res

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,6 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	// for (i = 0; i < n && src[i] != '\0'; i++)
-	// {
-	// 	dest[i] = src[i];
-	// }
-	// for ( ; i < n; i++)
-	// {
-	// 	dest[i] = '\0';
-	// }
-	// return (dest);
 	while (src[i] != '\0')
 	{
 		if (i < n)
