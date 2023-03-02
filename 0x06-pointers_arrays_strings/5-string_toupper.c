8889f30ca52ee46a051220f78411f1aebc35e64d
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 17:43:52 +0200

    for

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,14 @@
 **/
 char *string_toupper(char *str)
 {
-	while (*str != '\0')
-	{
-		if (*str >= 'a' && *str <= 'z')
-		{
-			*str -= 32;
-		}
-		str++;
-	}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
 	return (str);
 }
