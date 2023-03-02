f6debbee70abf239f0ad460eec1f06a5e5800761
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 23:30:51 +0200

    del

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
-* _strlen - function that returns the length of a string.
-* @s : s is a character
-* Return: value is i
-**/
-
-int _strlen(char *s)
-{
-int len = 0;
-while (*s++)
-len++;
-return (len);
-}
-
-/**
  * infinite_add - Adds two numbers
  * @n1: The first number
  * @n2: The second number
@@ -23,49 +9,35 @@ return (len);
  *
  * Return: If result can be stored in r, then r, otherwise 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int r_index)
 {
-  int carry = 0;
-  int i = _strlen(n1) - 1;
-  int j = _strlen(n2) - 1;
-  int k = size_r - 1;
-  
-  r[k] = '\0';
+  int num, tens = 0;
 
-  while (i >= 0 || j >= 0) 
+  for (; *n1 && *n2; n1--, n2--, r_index--)
   {
-  int sum = carry;
-  if (i >= 0)
-    {
-      sum += n1[i] - '0';
-      i--;
-    }
-    if (j >= 0)
-    {
-      sum += n2[j] - '0';
-      j--;
-    }
-    if (k <= 0)
-    {
-      return 0;
-    }
-    carry = sum / 10;
-    r[k - 1] = sum % 10 + '0';
-    k--;
+    num = (*n1 - '0') + (*n2 - '0');
+    num += tens;
+    *(r + r_index) = (num % 10) + '0';
+    tens = num / 10;
   }
-
-  if (carry != 0)
+  for (; *n1; n1--, r_index--)
   {
-    if (k <= 0)
-    {
-      return 0;
-    }
-    r[k - 1] = carry + '0';
+    num = (*n1 - '0') + tens;
+    *(r + r_index) = (num % 10) + '0';
+    tens = num / 10;
   }
-  else if (k == size_r - 1)
+  for (; *n2; n2--, r_index--)
   {
-    return 0;
+    num = (*n2 - '0') + tens;
+    *(r + r_index) = (num % 10) + '0';
+    tens = num / 10;
   }
-
-  return &r[k];
+  if (tens && r_index >= 0)
+  {
+    *(r + r_index) = (tens % 10) + '0';
+    return (r + r_index);
+  }
+  else if (tens && r_index < 0)
+    return (0);
+  return (r + r_index + 1);
 }
