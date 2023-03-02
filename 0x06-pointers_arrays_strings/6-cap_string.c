d4d1e631e90970282f6582d4d6b99301633ad663
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-02 19:30:59 +0200

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* *string_toupper - changes all lowercase letters of a string to uppercase
+* *cap_string - changes all lowercase letters of a string to uppercase
 * @str: string
 * Return: string
 **/
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i = 0;
 
