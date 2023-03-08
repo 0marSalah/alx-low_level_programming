52a4bca30409999b5f21d4362ef451537338d822
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-08 20:13:02 +0200

    x

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,5 @@
 #include "main.h"
 
-/**
-* _sqrt_recursion - returns natural square root of a number
-* @n: integer
-* Return: natural square root or -1 if no square root for n
-**/
-int _sqrt_recursion(int n)
-{
-return (natural_square(n, 1));
-}
 
 /**
 * natural_square - a function that help to find natural_square
@@ -26,4 +17,14 @@ int natural_square(int x, int y)
 	if (s == x)
 		return (y);
 	return (natural_square(x, y + 1));
+}
+
+/**
+* _sqrt_recursion - returns natural square root of a number
+* @n: integer
+* Return: natural square root or -1 if no square root for n
+**/
+int _sqrt_recursion(int n)
+{
+return (natural_square(n, 1));
 }
