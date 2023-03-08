0d584425c269c809d04a048026c9bfcacfe2bb8e
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-08 20:34:59 +0200

    lm

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,11 +21,14 @@ return (1 + _strlen(s + 1));
 * Return: recursively check
 **/
 
-int is_palindrome_helper(char *s, int start, int end) {
-	if (start >= end) {
+int is_palindrome_helper(char *s, int start, int end) 
+{
+	if (start >= end) 
+	{
 		return 1;
 	}
-	if (s[start] != s[end]) {
+	if (s[start] != s[end]) 
+	{
 		return 0;
 	}
 	return is_palindrome_helper(s, start+1, end-1);
@@ -36,7 +39,8 @@ int is_palindrome_helper(char *s, int start, int end) {
 * Return: 1 if palindrome or 0 if not
 **/
 
-int is_palindrome(char *s) {
+int is_palindrome(char *s)
+{
 	int len = _strlen(s);
 	return is_palindrome_helper(s, 0, len-1);
 }
