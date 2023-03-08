8019c1a62b353561c04796c847f329b3e67492c3
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-08 15:19:34 +0200

    assas

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,10 @@ void _puts_recursion(char *s)
 {
   if (*s == "\0")
   {
-    _putchar('\n');
+    printf('\n');
     return;
   }
-  _putchar(*s);
-  _puts_recursion(s + 1);
+  printf("%c", *s);
+  s++;
+  _puts_recursion(s);
 }
