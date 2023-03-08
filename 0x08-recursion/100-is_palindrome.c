6e168f4dbb54856c0569de82251bb377fb1cab06
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-08 20:40:11 +0200

    ddd

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,13 +25,13 @@ int is_palindrome_helper(char *s, int start, int end)
 {
 if (start >= end)
 {
-	return 1;
+return 1;
 }
 if (s[start] != s[end])
 {
-	return 0;
+return 0;
 }
-return is_palindrome_helper(s, start+1, end-1);
+return (is_palindrome_helper(s, start + 1, end - 1));
 }
 /**
 * is_palindrome - a function that returns 1 if a string is a palindrome and 0 if not.
@@ -42,5 +42,5 @@ return is_palindrome_helper(s, start+1, end-1);
 int is_palindrome(char *s)
 {
 int len = _strlen(s);
-return is_palindrome_helper(s, 0, len-1);
+return (is_palindrome_helper(s, 0, len - 1));
 }
