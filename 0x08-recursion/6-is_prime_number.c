62cc1ca7321f6f0bbd85ffd9f3bd9a9486c43696
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-08 20:12:15 +0200

    x

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
-* is_prime_number - returns a prime number
-* @n: takes in an integer
-* Return: 1 if n a prime number or 0 if not prime
-**/
-int is_prime_number(int n)
-{
-if (n == 1 || n < 0)
-return (0);
-return (pnumber(2, n));
-
-}
-/**
 * pnumber - help me to find the prime number
 * @x: integer
 * @n: integer
@@ -24,4 +12,16 @@ int pnumber(int x, int n)
 	else if (n % x == 0)
 		return (0);
 	return (pnumber(x + 1, n));
+}
+/**
+* is_prime_number - returns a prime number
+* @n: takes in an integer
+* Return: 1 if n a prime number or 0 if not prime
+**/
+int is_prime_number(int n)
+{
+if (n == 1 || n < 0)
+return (0);
+return (pnumber(2, n));
+
 }
