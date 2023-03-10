cb43dbec728b895773b68ec791aa90e82532f299
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:12:00 +0200

    FIXED

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,13 +10,8 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	if(argc > 0)
-	{
-		printf("%s\n", argv[0]);
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
