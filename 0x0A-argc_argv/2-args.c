eb7719f2c4b3abee2666f1e9c7ee7953257e3e0c
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:08:47 +0200

    ff

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,16 +7,13 @@
 * @argv: array
 * Return: 0
 **/
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
-	if (argc > 0)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s", argv[i]);
-			printf("\n");
-		}
+		printf("%s", argv[i]);
+		printf("\n");
 	}
 	return (0);
 }
