54d3e8b2ec2a000220c3b1c265b11d92ce9988ec
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:04:47 +0200

    gv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,8 @@
 * @argv: array
 * Return: 0
 **/
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
