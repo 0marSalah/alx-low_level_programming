b4c2a75a47592327df1665865418bfcfb66853fe
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:51:51 +0200

    ha3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,27 +3,34 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    int sum = 0;
-    int i, j;
-    char *ptr;
-    
-    // Loop through each argument passed to the program
-    for (i = 1; i < argc; i++) {
-        // Loop through each character in the argument
-        for (j = 0; argv[i][j] != '\0'; j++) {
-            // Check if the character is a digit or not
-            if (!isdigit(argv[i][j])) {
-                // If not, print an error message and return 1
-                printf("Error\n");
-                return 1;
-            }
-        }
-        
-        // Convert the argument to an integer and add it to the sum
-        sum += strtol(argv[i], &ptr, 10);
-    }
-    
-    // Print the sum or 0 if no arguments were passed
-    printf("%d\n", sum);
-    return 0;
+    int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			for (j = 1; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
