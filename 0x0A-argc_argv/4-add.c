68d6eeb4d0fbab86876efb6ae6eecdb7296e4ed3
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:48:50 +0200

    lml

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,15 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/**
-* main - entry point
-* @argc: size of argv
-* @argv: array
-* Return: 0
-**/
-int main(int argc, char *argv[])
-{
-	int sum = 0;
+int main(int argc, char *argv[]) {
+    int sum = 0;
     int i, j;
     char *ptr;
     
