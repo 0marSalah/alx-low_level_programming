4f6ebb0b8dd051cd32c14d0c30a1f62af487b4de
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 17:11:03 +0200

    DCDDDDDDDDDDDDDDDDDDD

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,10 +11,12 @@ int main(int argc, char *argv[])
 {
 	int i;
 	if(argc > 0)
+	{
 		printf("%s\n", argv[0]);
 		for (i = 1; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
 		}
+	}
 	return (0);
 }
