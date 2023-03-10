e372d23528c32e40f6c3a5572f4be15824c52715
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 16:15:03 +0200

    wow

---
+++ 0x09-static_libraries/_strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of a string.
+* @s : s is a character
+* Return: value is i
+**/
+
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
