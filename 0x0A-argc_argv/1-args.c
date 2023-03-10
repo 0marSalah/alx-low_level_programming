b24da2b56a6c55b552caeffa3ee0814d39f961e0
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 16:57:34 +0200

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
