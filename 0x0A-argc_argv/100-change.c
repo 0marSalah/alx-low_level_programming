3057c690b4310a88385eba4bd34ad925e7e2be67
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-10 18:22:16 +0200

    d

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+    int cents, coins;
+
     if (argc != 2) {
         printf("Error\n");
         return 1;
     }
 
-    int cents = atoi(argv[1]);
+    cents = atoi(argv[1]);
 
     if (cents < 0) {
         printf("0\n");
         return 0;
     }
 
-    int coins = 0;
+    coins = 0;
     coins += cents / 25; cents %= 25;
     coins += cents / 10; cents %= 10;
     coins += cents / 5; cents %= 5;
