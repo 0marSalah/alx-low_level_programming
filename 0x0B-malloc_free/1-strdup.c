f71f7c51368c91c2eb70064f654c5093cc73068d
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-13 17:48:44 +0200

    whalaaaa agaaain

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* _strdup - a function that returns a pointer to a newly 
+* _strdup - a function that returns a pointer to a newly
 * allocated space in memory
 * @str: char
 * Return: pointer to the duplicated string
