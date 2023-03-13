39ecdac3d5e13d08620a877ec1da02dc26a77a53
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-13 18:15:33 +0200

    gcc mafucker

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@
 **/
 char *str_concat(char *s1, char *s2)
 {
+char *result;
 if (s1 == NULL)
 {
 s1 = "";
@@ -20,7 +21,7 @@ if (s2 == NULL)
 s2 = "";
 }
 
-char *result = malloc(strlen(s1) + strlen(s2) + 1);
+result = malloc(strlen(s1) + strlen(s2) + 1);
 
 if (result == NULL)
 {
