bc24f7abdf565efdbca4d279be1d6e474147a8a7
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-13 17:45:05 +0200

    last

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,12 +9,12 @@ char *_strdup(char *str)
   if (str == NULL)
     return (NULL);
 
-  str_p = malloc(strlen(str) + 1)
+  str_p = malloc(strlen(str) + 1);
 
   if (str_p == NULL)
     return (NULL);
 
-  strcpy(dup, str);
+  strcpy(str_p, str);
 
   return (str_p);
 }
