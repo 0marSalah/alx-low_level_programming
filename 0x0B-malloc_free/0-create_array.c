5130fe83cc8cf6a5d56ebb7119ffa6113bc18d46
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-13 17:02:17 +0200

    betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,25 +3,25 @@
 
 
 /**
- **create_array -  a function that creates an array of chars, and initializes it with a specific char.
+ **create_array -  a function that creates an array of chars
  * @size - positive integer number
  * @c - character
  */
 
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *array;
-  if (size == 0) 
-    return (NULL);
+unsigned int i;
+char *array;
+if (size == 0)
+return (NULL);
 
-  array = malloc(size * sizeof(char));
-  if (array == NULL)
-    return (NULL);
+array = malloc(size * sizeof(char));
+if (array == NULL)
+return (NULL);
 
-  for (i = 0; i < size; i++)
-  {
-    array[i] = c;
-  }
-  return (array);
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
 }
