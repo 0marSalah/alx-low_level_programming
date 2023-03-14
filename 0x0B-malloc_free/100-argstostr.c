b3934f3138b2f9c8d1b295cff0ea28ab772bd981
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-14 20:15:29 +0200

    declarations

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h> // for malloc and free
+#include <stdlib.h>
 
 /**
 * argstostr - a function that concatenates all the arguments of your program.
@@ -9,40 +9,42 @@
 **/
 
 char *argstostr(int ac, char **av) {
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
-int len = 0;
-char *result;
-for (int i = 0; i < ac; i++)
-{
-int j = 0;
-while (av[i][j] != '\0')
-{
-len++;
-j++;
-}
-len++;
-}
-result = malloc((len + 1) * sizeof(char));
-if (result == NULL)
-{
-return (NULL);
-}
-int k = 0;
-for (int i = 0; i < ac; i++)
-{
-while (av[i][j] != '\0')
-{
-result[k] = av[i][j];
-k++;
-j++;
-}
-result[k] = '\n';
-k++;
-}
-result[k] = '\0';
-return (result);
+  int len = 0;
+  char *result;
+  int j;
+  int i;
+  int k = 0;
+  if (ac == 0 || av == NULL)
+  {
+    return (NULL);
+  }
+  for (i = 0; i < ac; i++)
+  {
+    j = 0;
+    while (av[i][j] != '\0')
+    {
+      len++;
+      j++;
+    }
+    len++;
+  }
+  result = malloc((len + 1) * sizeof(char));
+  if (result == NULL)
+  {
+    return (NULL);
+  }
+  for (i = 0; i < ac; i++)
+  {
+    while (av[i][j] != '\0')
+    {
+      result[k] = av[i][j];
+      k++;
+      j++;
+    }
+    result[k] = '\n';
+    k++;
+  }
+  result[k] = '\0';
+  return (result);
 }
 
