0471684c2d3bf1693552d3e00c64adc135e083ef
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-14 17:01:21 +0200

    again

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* free_grid - a function that frees 
+* free_grid - a function that frees
 * a 2 dimensional grid previously created by your alloc_grid function.
 * @grid: a function that returns a pointer to
 * a 2 dimensional array of integers.
