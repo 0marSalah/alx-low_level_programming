b3642da4826d3935167116b7e8f277477c675b80
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-16 14:28:14 +0200

    bett done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
 * string_nconcat - a function that concatenates two strings
 * @n: unsigned int
@@ -11,35 +10,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  int l, i;
-	unsigned int j;
-	char *p;
+int l, i;
+unsigned int j;
+char *p;
 
-	l = 0;
-	i = 0;
-	j = 0;
+l = 0;
+i = 0;
+j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[l])
-		l += 1;
-	p = malloc(l + n + 1);
-	if (p == NULL)
-		return (NULL);
-	while (s1[i])
-	{
-		p[i] = s1[i];
-		i += 1;
-	}
-
-	while (j < n)
-	{
-		p[i + j] = s2[j];
-		j += 1;
-	}
-	p[i + j] = '\0';
-	return (p);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[l])
+l += 1;
+p = malloc(l + n + 1);
+if (p == NULL)
+return (NULL);
+while (s1[i])
+{
+p[i] = s1[i];
+i += 1;
 }
 
+while (j < n)
+{
+p[i + j] = s2[j];
+j += 1;
+}
+p[i + j] = '\0';
+return (p);
+}
