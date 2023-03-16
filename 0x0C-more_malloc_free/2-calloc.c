4a01c28d38701519c632e11747710b087f85232d
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-16 14:48:18 +0200

    testing

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* string_nconcat - a function that concatenates two strings
+* @n: unsigned int
+* @s1: char
+* @s2: char
+* Return: Returns a pointer to the allocated memory
+**/
+
+char *_calloc(unsigned int nmemb, unsigned int size)
+{
+  void *res;
+  char *p;
+  int i;
+
+  if (!nmemb || !size)
+    return (NULL);
+
+  res = malloc(size * nmemb);
+
+  if (!res)
+    return (NULL);
+
+  p = res;
+
+  for (i = 0; i < nmemb * size; i++) {
+    p[i] = 0;
+  }
+
+  return (res);
+}
