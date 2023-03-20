d571f1d0e94f61bd77ba50f553f82a9cd97769a6
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-20 21:32:25 +0200

    teddy

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
-#define ABS(x)(((x) < (0)) ? ((x) * (-1)) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
