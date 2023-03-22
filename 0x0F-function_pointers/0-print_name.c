059143f954352b39c399020d13e616e65753d313
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-22 16:27:00 +0200

    teddy

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - prints a name.
  * @name: input name.
  * @f: function pointer.
+ *
  * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (f && name)
-f(name);
+	if (name && f)
+		f(name);
 }
