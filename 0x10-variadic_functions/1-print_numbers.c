e94acf851ee662f8bb74f5c5f81830bf76ea2536
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-23 22:47:20 +0200

    fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * sum_them_all - returns the sum of all its parameters.
@@ -19,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{	
     printf("%d" , va_arg(valist, int));
-    if (separator)
+    if (separator && i < n - 1)
       printf ("%s", separator);
   }
 
-	va_end(valist);
   printf("\n");
+	va_end(valist);
 }
