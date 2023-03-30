b716d00ea21700ad36b21e416af83d4d527cd2da
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-30 22:37:10 +0200

    testing

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* myStartupFun - a function that execute before main function
+* Return: nothing
+**/
+
+void myStartupFun(void)
+{
+  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+void myStartupFun(void) __attribute__ ((constructor));
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
