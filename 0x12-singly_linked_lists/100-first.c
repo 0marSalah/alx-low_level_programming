4015125107996e247143b11e31b380a5e63686c9
OmarSalah1911 <omar.salah1597@gmail.com>
2023-03-30 22:38:01 +0200

    betty finish

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 
 void myStartupFun(void)
 {
-  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
