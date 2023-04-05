97126d65fbb7d265062df9ac73be31eef1e815f8
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-05 02:54:55 +0200

    testing 5

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,21 +1,39 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
- * @head: pointer to the head of the list
- * Return: the number of nodes in the list
- */
+* print_listint_safe - a function that prints a listint_t
+*linked list
+* @head: points to the beginning of a linked list
+* Return: the number of nodes in the list
+**/
+
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	const listint_t *tmp = head;
+	const listint_t *f_ptr, *s_ptr;
+	size_t size;
 
-	while (tmp != NULL)
+	size = 0;
+
+	if (head == NULL)
+		return (0);
+
+	s_ptr = head;
+	f_ptr = head->next;
+
+	while (f_ptr != NULL && f_ptr < s_ptr)
 	{
-		printf("[%p] %d\n", (void *)tmp, tmp->n);
-		tmp = tmp->next;
-		i++;
+		size += 1;
+	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+		s_ptr = s_ptr->next;
+		f_ptr = f_ptr->next;
 	}
-	return (i);
+	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+	size += 1;
+	if (f_ptr)
+		printf("-> [%p] %i\n", (void *)f_ptr, f_ptr->n);
+
+	return (size);
 }
