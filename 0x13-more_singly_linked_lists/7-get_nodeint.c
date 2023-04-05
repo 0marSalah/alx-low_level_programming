d58ef670656ce8a207113001d5caac04f1664fb4
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-05 03:12:38 +0200

    ok

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,30 +1,46 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* *get_nodeint_at_index - a function delete head of the list
-* @head: a head of linked_list
-* @index: unsigned int to index
-* Return: int
-**/
+* insert_nodeint_at_index - a function that inserts a new
+*node at a given position
+* @head: a pointer to a pointer to a struct
+* @idx: is the index of the list where the new node
+*should be added. Index starts at 0
+* @n: the data for the new node
+* Return: the address of the new node, or NULL if it failed
+*/
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int count;
-listint_t *ptr;
+	listint_t *next_ptr;
+	listint_t *previous_ptr;
+	listint_t *new_node;
+	unsigned int i;
 
-count = 0;
-ptr = head;
+	if (head == NULL)
+		return (NULL);
+	next_ptr = *head;
+	previous_ptr = NULL;
+	for (i = 0; i < idx; i += 1)
+	{
+		if (next_ptr == NULL)
+			return (NULL);
+		previous_ptr = next_ptr;
+		next_ptr = next_ptr->next;
+	}
 
-while (count < index)
-{
-if (ptr == NULL)
-return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = next_ptr;
+	if (idx == 0)
+		*head = new_node;
+	else
+		previous_ptr->next = new_node;
 
-ptr = ptr->next;
-count++;
-}
-return (ptr);
+	return (new_node);
 }
