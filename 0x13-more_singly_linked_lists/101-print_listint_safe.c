4f2d3a2a6791b162b544341fff31d5cce178632a
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-05 02:49:08 +0200

    testing 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,28 +10,28 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-  const listint_t *f_ptr, *s_ptr;
-  size_t size;
+	const listint_t *f_ptr, *s_ptr;
+	size_t size;
 
-  size = 0;
+	size = 0;
 
-  if (head == NULL)
-    return (0);
+	if (head == NULL)
+		return (0);
 
-  s_ptr = head;
-  f_ptr = head->next;
+	s_ptr = head;
+	f_ptr = head->next;
 
-  while (f_ptr && s_ptr > f_ptr)
-  {
-    size++;
-    printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
-    s_ptr = s_ptr->next;
-    f_ptr = f_ptr->next;
-  }
-  printf("-> [%p] %i\n", (void *)s_ptr, s_ptr->n);
-  size++;
-  if (f_ptr)
-  printf("-> [%p] %i\n", (void *)f_ptr, f_ptr->n);
+	while (f_ptr != NULL && f_ptr < s_ptr)
+	{
+		size += 1;
+	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+		s_ptr = s_ptr->next;
+		f_ptr = f_ptr->next;
+	}
+	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
+	size += 1;
+	if (f_ptr)
+		printf("-> [%p] %i\n", (void *)f_ptr, f_ptr->n);
 
-  return (size);
+	return (size);
 }
