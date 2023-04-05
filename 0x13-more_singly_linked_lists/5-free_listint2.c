bcc89e38275191ceee43e88fee80d27895f124ea
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-05 03:01:43 +0200

    freeptr tst 1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,30 @@
+
+ 
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* *free_listint2 - a function frees listint_t
-* @head: a head of linked_list
-* Return: nothing
-**/
+* free_listint2 - a func that frees a list
+* @head: a pointer to a pointer that point to a list
+*
+*/
 
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
-if (*head == NULL)
-return;
-while (*head != NULL)
-{
-tmp = *head;
-*head = (*head)->next;
-free(tmp);
-}
-*head = NULL;
+	listint_t *free_ptr;
+	listint_t *ptr;
+
+	if (head != NULL)
+	{
+		ptr = *head;
+		while ((free_ptr = ptr) != NULL)
+		{
+			ptr = ptr->next;
+			free(free_ptr);
+		}
+		*head = NULL;
+	}
 }
