7bbd4e499ab2f77cae6b41a11e6a9abb8b6062f4
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-08 01:05:26 +0200

    betty binary to deci

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,20 +11,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int num = 0;
-  int i = 0;
+unsigned int num = 0;
+int i = 0;
 
-  if (!b)
-    return (0);
+if (!b)
+return (0);
 
-  while (b[i] != '\0')
-  {
-    if (b[i] != '0' && b[i] != '1')
-      return (0);
-    num <<= 1;
-    if (b[i] == '1')
-      num = num ^ 1;
-    i++;
-  }
-  return (num);
+while (b[i] != '\0')
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+num <<= 1;
+if (b[i] == '1')
+num = num ^ 1;
+i++;
+}
+return (num);
 }
