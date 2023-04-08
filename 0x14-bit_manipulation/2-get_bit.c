18d0be8b1181fca785b2e97a7a997d9f7ecee3a3
OmarSalah1911 <omar.salah1597@gmail.com>
2023-04-08 02:13:22 +0200

    worked!!

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-  if (index > sizeof(n) * 8)
-    return (-1);
-  return ((n >> index) & 1);
+if (index > sizeof(n) * 8)
+return (-1);
+return ((n >> index) & 1);
 }
