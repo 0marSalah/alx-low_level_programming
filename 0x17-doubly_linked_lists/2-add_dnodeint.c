f84f20f44b0c6c6b5d996ae7e3f78884937681f5
0marSalah <omar.salah1597@gmail.com>
2023-05-18 14:15:02 +0300

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - return the number of elements in a linked dlistint_t list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+  size_t i;
+
+  for (i = 0; h != NULL; i++)
+    h = h->next;
+  return (i);
+}
+
+/**
  * add_dnodeint - add a new node at the beginning of a dlistint_t list
  * @head: pointer to the start of the linked list
  * @n: integer to add to the list
