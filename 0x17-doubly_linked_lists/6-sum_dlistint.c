e8afd6daf65755f35b905b076f1e81427099bc54
0marSalah <omar.salah1597@gmail.com>
2023-05-18 15:25:58 +0300

    return 0

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,11 +11,14 @@ int sum_dlistint(dlistint_t *head)
 {
 int sum = 0;
 
+if (head == NULL)
+return (0);
+
 while (head != "\0")
 {
 sum += head->n;
 head = head->next;
 }
 
-return sum;
+return (sum);
 }
