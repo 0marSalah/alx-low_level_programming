85c1fb6e26269c73ef3a39eea50e7ec34f241f94
0marSalah <omar.salah1597@gmail.com>
2023-05-18 14:26:05 +0300

    hell betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,9 +9,9 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t i;
+size_t i;
 
-  for (i = 0; h != NULL; i++)
-    h = h->next;
-  return (i);
+for (i = 0; h != NULL; i++)
+h = h->next;
+return (i);
 }
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,11 +9,11 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t i;
+size_t i;
 
-  for (i = 0; h != NULL; i++)
-    h = h->next;
-  return (i);
+for (i = 0; h != NULL; i++)
+h = h->next;
+return (i);
 }
 
 /**
@@ -26,23 +26,23 @@ size_t dlistint_len(const dlistint_t *h)
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *new;
+dlistint_t *new;
 
-  if (head == NULL)
-    return (NULL);
+if (head == NULL)
+return (NULL);
 
-  new = malloc(sizeof(dlistint_t));
-  if (new == NULL)
-    return (NULL);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
 
-  new->n = n;
-  new->prev = NULL;
-  new->next = *head;
+new->n = n;
+new->prev = NULL;
+new->next = *head;
 
-  if (*head != NULL)
-    (*head)->prev = new;
+if (*head != NULL)
+(*head)->prev = new;
 
-  *head = new;
+*head = new;
 
-  return (new);
+return (new);
 }
