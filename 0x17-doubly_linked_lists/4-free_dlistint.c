e832c94d80a6f8dfe2368155ce82458b461e565e
0marSalah <omar.salah1597@gmail.com>
2023-05-18 15:16:16 +0300

    betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *temp;
+dlistint_t *temp;
 
-  while (head != NULL)
-  {
-    temp = head->next;
-    free(head);
-    head = temp;
-  }
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
