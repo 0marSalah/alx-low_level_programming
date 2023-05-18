b4f413aa7cf1d5189f16bf11492e4e78fce53eb7
0marSalah <omar.salah1597@gmail.com>
2023-05-18 13:57:16 +0300

    fix my head

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -16,5 +17,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
