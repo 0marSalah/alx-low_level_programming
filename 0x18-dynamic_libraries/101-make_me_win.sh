002896b842b62a79fb08b1418f8e7a6ed4175241
0marSalah <omar.salah1597@gmail.com>
2023-05-22 23:47:46 +0300

    yarab (:)

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/usr/shell
-wget -P /tmp https://github.com/0marSalah/alx-low_level_programming/blob/main/0x18-dynamic_libraries/iwin.so
+wget -P /tmp https://raw.github.com/0marSalah/alx-low_level_programming/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
