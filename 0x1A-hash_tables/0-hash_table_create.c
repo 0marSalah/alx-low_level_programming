8178efd8165b2f0bd564031496ce3ac807909270
0marSalah <omar.salah1597@gmail.com>
2023-06-08 19:10:28 +0300

    wrong repo :)

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+  hash_table_t *new_table = NULL;
+
+  if (size == 0)
+    return (NULL);
+
+  new_table = malloc(sizeof(hash_table_t));
+  if (new_table == NULL)
+    return (NULL);
+
+  new_table->size = size;
+  new_table->array = calloc(size, sizeof(hash_node_t *));
+  if (new_table->array == NULL)
+  {
+    free(new_table);
+    return (NULL);
+  }
+
+  return (new_table);
+}
