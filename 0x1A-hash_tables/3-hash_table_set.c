3f5380b34ffc2e7d5d5f5f6f9fd75cce672fd651
0marSalah <omar.salah1597@gmail.com>
2023-06-08 22:33:34 +0300

    not safe

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value); /* strdup() returns a pointer to a new string */
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
