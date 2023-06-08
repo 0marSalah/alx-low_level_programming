84fcc3158318920c18cb692d50b3ad13d50a8c36
0marSalah <omar.salah1597@gmail.com>
2023-06-08 19:23:14 +0300

    another logic with malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new_table = NULL;
+  hash_table_t *new_table = NULL;
+  unsigned int i;
 
-if (size == 0)
-return (NULL);
+  if (size == 0)
+  return (NULL);
 
-new_table = malloc(sizeof(hash_table_t));
-if (new_table == NULL)
-return (NULL);
+  new_table = malloc(sizeof(hash_table_t));
+  if (new_table == NULL)
+    return (NULL);
 
-new_table->size = size;
-new_table->array = calloc(size, sizeof(hash_node_t *));
-if (new_table->array == NULL)
-{
-free(new_table);
-return (NULL);
-}
+  new_table->size = size;
+  new_table->array = malloc(size * sizeof(hash_node_t));
 
-return (new_table);
+  if (ht->array == NULL)
+  {
+    free(new_table);
+    return (NULL);
+  }
+  for (i = 0; i < size; i++)
+    new_table->array[i] = NULL;
+
+  return (new_table);
 }
