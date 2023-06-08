a56eb80a9360b2447216a868feeb51d7f3723bb5
0marSalah <omar.salah1597@gmail.com>
2023-06-08 23:02:56 +0300

    betty hell!!

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,23 +7,23 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-  unsigned long int i;
-  hash_node_t *node;
-  char *separator = "";
+unsigned long int i;
+hash_node_t *node;
+char *separator = "";
 
-  if (ht == NULL)
-    return;
+if (ht == NULL)
+return;
 
-  printf("{");
-  for (i = 0; i < ht->size; i++)
-  {
-    node = ht->array[i];
-    while (node != NULL)
-    {
-      printf("%s'%s': '%s'", separator, node->key, node->value);
-      separator = ", ";
-      node = node->next;
-    }
-  }
-  printf("}\n");
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+printf("%s'%s': '%s'", separator, node->key, node->value);
+separator = ", ";
+node = node->next;
+}
+}
+printf("}\n");
 }
