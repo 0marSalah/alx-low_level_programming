707f2d4d9f8c26e60af12534d47a7fff71a6a7a9
0marSalah <omar.salah1597@gmail.com>
2023-06-08 19:15:08 +0300

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table = NULL;
+hash_table_t *new_table = NULL;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  new_table = malloc(sizeof(hash_table_t));
-  if (new_table == NULL)
-    return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-  new_table->size = size;
-  new_table->array = calloc(size, sizeof(hash_node_t *));
-  if (new_table->array == NULL)
-  {
-    free(new_table);
-    return (NULL);
-  }
+new_table->size = size;
+new_table->array = calloc(size, sizeof(hash_node_t *));
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
 
-  return (new_table);
+return (new_table);
 }
