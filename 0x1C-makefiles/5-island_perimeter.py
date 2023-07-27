818248ccbb42ee9e9629f9245261e2a4b6ffca6c
0marSalah <omar.salah1597@gmail.com>
2023-07-27 14:12:39 +0300

    fix comments

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,19 +4,16 @@
 
 def island_perimeter(grid):
     """Return the perimeter of the island defined in grid. """
-    perimeter = 0  # Initialize perimeter
-
-    # Iterating through each cell in the grid
-    # (ignoring the boundary cells for simplicity)
+    perimeter = 0
     for row in range(1, len(grid) - 1):
         for col in range(1, len(grid[0]) - 1):
-            if grid[row][col] == 1:  # If the cell is land zone
-                if grid[row - 1][col] == 0:  # Check top cell
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0:
                     perimeter += 1
-                if grid[row + 1][col] == 0:  # Check bottom cell
+                if grid[row + 1][col] == 0:
                     perimeter += 1
-                if grid[row][col - 1] == 0:  # Check left cell
+                if grid[row][col - 1] == 0:
                     perimeter += 1
-                if grid[row][col + 1] == 0:  # Check right cell
+                if grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
