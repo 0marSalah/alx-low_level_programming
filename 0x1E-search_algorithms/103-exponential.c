e217c792dcb3ff7f781dfa320dd51898f89b0970
0marSalah <omar.salah1597@gmail.com>
2023-09-12 20:17:52 +0300

    first try

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -10,27 +10,27 @@
 */
 
 int binarySearch(int *array, size_t start, size_t end, int key) {
-  if(start <= end)
-  {
-    int mid = (start + (end - start) /2);
-    size_t i;
+if(start <= end)
+{
+int mid = (start + (end - start) /2);
+size_t i;
 
-    printf("Searching in array: ");
-    for (i = start; i <= end; i++)
-    {
-      if (i == end)
-        printf("%d", array[i]);
-      else
-        printf("%d, ", array[i]);
-    }
-    printf("\n");
-    if(array[mid] == key)
-        return mid;
-    if(array[mid] > key)
-      return binarySearch(array, start, mid-1, key);
-    return binarySearch(array, mid+1, end, key);
-  }
-  return (-1);
+printf("Searching in array: ");
+for (i = start; i <= end; i++)
+{
+if (i == end)
+printf("%d", array[i]);
+else
+printf("%d, ", array[i]);
+}
+printf("\n");
+if(array[mid] == key)
+return mid;
+if(array[mid] > key)
+return binarySearch(array, start, mid-1, key);
+return binarySearch(array, mid+1, end, key);
+}
+return (-1);
 }
 
 /**
@@ -44,20 +44,20 @@ int binarySearch(int *array, size_t start, size_t end, int key) {
 
 int exponential_search(int *array, size_t size, int value)
 {
-  size_t i = 1;
+size_t i = 1;
 
-  if (array == NULL)
-    return (-1);
+if (array == NULL)
+return (-1);
 
-  if (array[0] == value)
-    return (0);
-  
-  while (i < size && array[i] <= value)
-  {
-    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-    i = i * 2;
-  }
-  printf("Value found between indexes [%lu] and [%lu]\n", i/2, i > size - 1 ? size - 1 : i);
+if (array[0] == value)
+return (0);
 
-  return (binarySearch(array, i/2, i > size - 1 ? size - 1 : i, (int)value));
+while (i < size && array[i] <= value)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+i = i * 2;
+}
+printf("Value found between indexes [%lu] and [%lu]\n", i/2, i > size - 1 ? size - 1 : i);
+
+return (binarySearch(array, i/2, i > size - 1 ? size - 1 : i, (int)value));
 }
