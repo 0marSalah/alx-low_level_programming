e8324dd2231a0f2b5df0941673db884b93088979
0marSalah <omar.salah1597@gmail.com>
2023-09-12 12:43:30 +0300

    print before condition

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,9 +18,9 @@ int linear_search(int *array, size_t size, int value)
     return (-1);
   for (i = 0; i < size; i++)
   {
+    printf("Value checked array[%ld] = [%d]\n", i, array[i]);
     if (array[i] == value)
       return (i);
-    printf("Value checked array[%ld] = [%d]\n", i, array[i]);
   }
   return (-1);
 }
