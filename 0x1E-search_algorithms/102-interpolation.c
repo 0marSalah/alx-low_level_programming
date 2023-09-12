0667133d6163bb1927b9317ec7aa27562367a9f2
0marSalah <omar.salah1597@gmail.com>
2023-09-12 17:41:32 +0300

    fix betty

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,27 +11,27 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-  size_t lo = 0, hi = size - 1, mid = -1;
+size_t lo = 0, hi = size - 1, mid = -1;
 
-  while (lo <= hi)
-  {
-    mid = (int)(lo + (hi - lo) * (value - array[lo]) / (array[hi] - array[lo]));
+while (lo <= hi)
+{
+mid = (int)(lo + (hi - lo) * (value - array[lo]) / (array[hi] - array[lo]));
 
-    if (mid < size)
-      printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
-    else
-    { 
-      printf("Value checked array [%lu] is out of bounds\n", mid);
-      break;
-    }
+if (mid < size)
+printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
+else
+{ 
+printf("Value checked array [%lu] is out of bounds\n", mid);
+break;
+}
 
-    if (array[mid] == value)
-      return (mid);
-    else if (array[mid] < value)
-      lo = mid + 1;
-    else
-      hi = mid - 1;
-  }
+if (array[mid] == value)
+return (mid);
+else if (array[mid] < value)
+lo = mid + 1;
+else
+hi = mid - 1;
+}
 
-  return (-1);
+return (-1);
 }
